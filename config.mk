ec73155a8cc8da450b1f3c5ce7284c625f5bcd42
Jürgen Urban <JuergenUrban@gmx.de>
2012-01-06 23:47:35 +0000

    Improved configuration. It is now easier to change debug output type.

--- config.mk
+++ config.mk
@@ -2,8 +2,12 @@ PS2LINUXDVD = /media/cdrom
 TARGET_IP = 192.168.0.23
 EXAMPLE_ELF = ../hello/hello.elf
 
-# Debug output using shared memory (working without RPC).
-SHARED_MEM_DEBUG = no
+# Set debug output type:
+# 1. none - no debug output
+# 2. shared - sharedmem.irx via ps2link
+# 3. fileio - SIF RPC stdout
+# 4. callback - Call function registered by Linux kernel (printk, dmesg).
+DEBUG_OUTPUT_TYPE = callback
 
 # Reset IOP at start (only working when enabled)
 RESET_IOP = yes
@@ -16,9 +20,6 @@ LOAD_PS2LINK = no
 LOAD_NAPLINK = no
 NAPLINK_PATH = ../../naplink-ps2-v1.0.1a/cd/
 
-# Debug output using fileio functions (RPC).
-FILEIO_DEBUG = no
-
 # Use new ROM modules in loader.
 NEW_ROM_MODULES = no
 
@@ -28,5 +29,26 @@ SCREENSHOT = yes
 # Activate debug for SBIOS (has only effect with shared memory debug or callback debug).
 SBIOS_DEBUG = no
 
+
+### Don't change the following part, change DEBUG_OUTPUT_TYPE instead.
+
+ifeq ($(DEBUG_OUTPUT_TYPE),shared)
+# Debug output using shared memory (working without RPC).
+SHARED_MEM_DEBUG = yes
+else
+SHARED_MEM_DEBUG = no
+endif
+
+ifeq ($(DEBUG_OUTPUT_TYPE),fileio)
+# Needs to be disabled when SHARED_MEM_DEBUG is active.
+FILEIO_DEBUG = yes
+else
+FILEIO_DEBUG = no
+endif
+
+ifeq ($(DEBUG_OUTPUT_TYPE),callback)
 # Activate printf callback in SBIOS
 CALLBACK_DEBUG = yes
+else
+CALLBACK_DEBUG = no
+endif
